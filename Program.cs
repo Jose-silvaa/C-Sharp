a26aab0fd2a85a61c0e14343ab0fa4f929fca8b3
Jose-silvaa <jvitorfacanha@gmail.com>
2025-06-28 16:31:03 -0300

    feat: começando c#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string firstFriend = "Maria";
+
+firstFriend = firstFriend.Trim();
+string secondFriend = "Jose";
+
+string friends = $"My friends are {firstFriend} and {secondFriend}";
+
+Console.WriteLine(friends);
+Console.WriteLine(friends.Replace("Maria", "Ana"));
+Console.WriteLine(friends.Contains("Maria"));
+
+Console.WriteLine(friends.Length);
+
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
