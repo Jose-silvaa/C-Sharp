7c7042a082dbba97351ef63d659b2f32849a9b70
Jose-silvaa <jvitorfacanha@gmail.com>
2025-07-02 19:22:13 -0300

    LINQ

--- obj/Debug/net9.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net9.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac4d409536a619ef556f79ca43cf3e9f9ccdd914")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8c77745d8a2fd9b9854495120c5234f0ba573f")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
